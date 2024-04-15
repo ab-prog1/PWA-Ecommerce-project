3392d13bae5c733bd7788ab2793c38cd3f0d683d
Abolfazl <ab.ghorbani28@gmail.com>
2024-04-15 14:18:50 +0200

    Config Routing

---
+++ frontend/ecom/src/route/AppRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+
+function AppRoute() {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<HomePage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default AppRoute;

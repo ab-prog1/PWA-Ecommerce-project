a8ccf68216764ee567e026b709018ef9bd4c8ed0
Abolfazl <ab.ghorbani28@gmail.com>
2024-05-06 13:51:49 +0200

    Consume Category Subcategory API Part 3

--- frontend/ecom/src/components/home/MegaMenu.jsx
+++ frontend/ecom/src/components/home/MegaMenu.jsx
@@ -7,46 +7,40 @@ class MegaMenu extends Component {
            
      }
  
+ 
 
-     MegaMenu(){
-          var acc = document.getElementsByClassName("accordion");
-          var accNum = acc.length;
-          var i;
-          for(i=0;i<accNum;i++){
-               acc[i].addEventListener("click",function (){
-                    this.classList.toggle("active");
-                    var panel = this.nextElementSibling;
-                    if(panel.style.maxHeight){
-                         panel.style.maxHeight = null;
-                    }else{
-                         panel.style.maxHeight= panel.scrollHeight+ "px"
-                    }
-               })
+     MenuItemClick=(event)=>{
+          event.target.classList.toggle("active");
+          var panel = event.target.nextElementSibling;
+          if(panel.style.maxHeight){
+               panel.style.maxHeight = null;
+          }else{
+               panel.style.maxHeight= panel.scrollHeight+ "px"
           }
+
      }
 
 
      render() {
 
           const CatList = this.props.data;
-
-          const MyView = CatList.map((CatList,i)=>{
-               return <div key={i.toString()}>
-      <button className="accordion">
-      <img className="accordionMenuIcon" src={CatList.category_image} />&nbsp; {CatList.category_name}
-                        </button>
-          <div className="panel">
-      <ul>
-          <li><a href="#" className="accordionItem" > Mans Tshirt 1</a></li>
-          <li><a href="#" className="accordionItem" > Mans Tshirt 2</a></li>
-      </ul>
-         </div> 
-             
-               </div>
-
-
-
-          });
+          const MyView = CatList.map((CatList, i) => {
+               return (
+                 <div key={CatList.category_name}>
+                   <button onClick={this.MenuItemClick} className="accordion">
+                     <img className="accordionMenuIcon" src={CatList.category_image} />&nbsp; {CatList.category_name}
+                   </button>
+                   <div className="panel">
+                     <ul>
+                       {CatList.subcategory_name.map((SubList, i) => {
+                         return <li key={SubList.subcategory_name}><a href="#" className="accordionItem">{SubList.subcategory_name}</a></li>
+                       })}
+                     </ul>
+                   </div>
+                 </div>
+               );
+             });
+  
 
 
 

7e62107d0800379e820beafdc4e40856f1cae676
Abolfazl <ab.ghorbani28@gmail.com>
2024-05-15 14:29:30 +0200

    Managing Loader Placeholder Part 4

--- frontend/ecom/src/components/home/Collection.jsx
+++ frontend/ecom/src/components/home/Collection.jsx
@@ -2,13 +2,16 @@ import React, { Component, Fragment } from 'react'
 import {Container,Row,Col,Card} from 'react-bootstrap'
 import AppURL from '../../api/AppURL';
 import axios from 'axios'
+import CollectionLoading from '../PlaceHolder/CollectionLoading';
 
 class Collection extends Component {
 
      constructor(){
           super();
           this.state={
-               ProductData:[]               
+               ProductData:[],
+               isLoading:"",
+               mainDiv:"d-none"               
           }
      }
 
@@ -16,7 +19,8 @@ class Collection extends Component {
      componentDidMount(){
           axios.get(AppURL.ProductListByRemark("COLLECTION")).then(response =>{
                
-               this.setState({ProductData:response.data});         
+               this.setState({ProductData:response.data,isLoading:"d-none",
+               mainDiv:" "});         
 
           }).catch(error=>{
 
@@ -30,7 +34,7 @@ class Collection extends Component {
           const MyView = CollectionList.map((CollectionList,i)=>{
 
           if(CollectionList.special_price=="na"){
-               return   <Col key={i.toString()} className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+               return   <Col className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
                <Card className="image-box card w-100">
                <img className="center w-75" src={CollectionList.image} />   
                <Card.Body> 
@@ -64,6 +68,10 @@ class Collection extends Component {
 
           return (
               <Fragment>
+                   <CollectionLoading  isLoading={this.state.isLoading} />
+
+                   <div className={this.state.mainDiv}>
+
                    <Container className="text-center" fluid={true}>
           <div className="section-title text-center mb-55"><h2> PRODUCT COLLECTION</h2>
           <p>Some Of Our Exclusive Collection, You May Like</p>
@@ -73,6 +81,7 @@ class Collection extends Component {
                {MyView}
      </Row>
                    </Container>
+                   </div>
               </Fragment>
           )
      }

8a5abba5b84db5d806d83374234d32083c9556a2
Abolfazl <ab.ghorbani28@gmail.com>
2024-05-17 12:28:05 +0200

    Create Product Details API Part 2

--- backend/adminecom/app/Http/Controllers/Admin/ProductDetailsController.php
+++ backend/adminecom/app/Http/Controllers/Admin/ProductDetailsController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ProductList;
+use App\Models\ProductDetails;
 
 class ProductDetailsController extends Controller
 {
-    //
+   public function ProductDetails(Request $request){
+   $id = $request->id;
+
+   $productDetails = productDetails::where ('product_id',$id)->get();
+   $productList = productList::where('id', $id)->get();
+
+   $item = [
+    'productDetails' => $productDetails,
+    'productList' => $productList,
+   ];
+   return $item;
+   }
+
 }
--- backend/adminecom/routes/api.php
+++ backend/adminecom/routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Admin\SiteInfoController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductListController;
 use App\Http\Controllers\Admin\SliderController;
-
+use App\Http\Controllers\Admin\ProductDetailsController;
 
 // get visitor
 Route::get('/getvisitor', [VisitorController::class, 'GetVisitorDetails']);
@@ -27,3 +27,5 @@ Route::get('/productlistbysubcategory/{category}/{subcategory}', [ProductListCon
 
 // Slider  route
 Route::get('/allslider', [SliderController::class, 'AllSlider']);
+// Prodouct Details  route
+Route::get('/productdetails/{id}', [ProductDetailsController::class, 'ProductDetails']);

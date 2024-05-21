77035b32a6ab1af30d2373b38d672c9d5dd94617
Abolfazl <ab.ghorbani28@gmail.com>
2024-05-21 10:35:18 +0200

    87 Consume Product Details API Part 1

--- frontend/ecom/src/pages/ProductCategoryPage.jsx
+++ frontend/ecom/src/pages/ProductCategoryPage.jsx
@@ -1,144 +1,60 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import AppURL from '../api/AppURL';
-import FooterDesktop from '../components/common/FooterDesktop';
-import FooterMobile from '../components/common/FooterMobile';
-import NavMenuDesktop from '../components/common/NavMenuDesktop';
-import NavMenuMobile from '../components/common/NavMenuMobile';
-import Category from '../components/ProductDetails/Category';
-import axios from 'axios';
+import React, { Component } from 'react'
+import { Fragment } from 'react'
+import AppURL from '../api/AppURL'
+import FooterDesktop from '../components/common/FooterDesktop'
+import FooterMobile from '../components/common/FooterMobile'
+import NavMenuDesktop from '../components/common/NavMenuDesktop'
+import NavMenuMobile from '../components/common/NavMenuMobile'
+import Category from '../components/ProductDetails/Category'
+import axios from 'axios'
 
-const ProductCategoryPage = () => {
-  const { category } = useParams();
-  const [productData, setProductData] = useState([]);
+ class ProductCategoryPage extends Component {
 
-  useEffect(() => {
-    window.scroll(0, 0);
+     constructor({match}){
+          super(); 
+          this.state={
+               Category:match.params.category,
+               ProductData:[] 
+          }
+     }
 
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(AppURL.ProductListByCategory(category));
-        setProductData(response.data);
-      } catch (error) {
-        console.error('Error fetching product data:', error);
-      }
-    };
-
-    fetchData();
-  }, [category]);
-
-  return (
-    <Fragment>
-      <div className="Desktop">
-        <NavMenuDesktop />
-      </div>
-
-      <div className="Mobile">
-        <NavMenuMobile />
-      </div>
-
-      <Category Category={category} ProductData={productData} />
-
-      <div className="Desktop">
-        <FooterDesktop />
-      </div>
-
-      <div className="Mobile">
-        <FooterMobile />
-      </div>
-    </Fragment>
-  );
-};
-
-export default ProductCategoryPage;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component, Fragment, useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import AppURL from '../api/AppURL'
-// import FooterDesktop from '../components/common/FooterDesktop'
-// import FooterMobile from '../components/common/FooterMobile'
-// import NavMenuDesktop from '../components/common/NavMenuDesktop'
-// import NavMenuMobile from '../components/common/NavMenuMobile'
-// import Category from '../components/ProductDetails/Category'
-// import axios from 'axios'
-
-// //  class ProductCategoryPage extends Component {
-//      const ProductCategoryPage = () => {
-//           const { category } = useParams();
-//           const [ProductData, setProductData] = useState([]);
-        
-//           useEffect(() => {
-//             window.scroll(0,0);
-          
-//             axios.get(AppURL.ProductListByCategory(Category)).then(response =>{
-//              this.setState({setProductData:response.data});         
-     
-//          }).catch(error=>{
-     
-//              });
-//           }, [category]);
-        
-
-     
-//           return (
-//               <Fragment> 
-//                <div className="Desktop">
-//                 <NavMenuDesktop /> 
-//                </div>
-
-//                <div className="Mobile">
-//                <NavMenuMobile />  
-//                </div>                       
-
-//                <Category Category={Category} ProductData={ProductData} /> 
-             
-
-//                <div className="Desktop">
-//                <FooterDesktop/>
-//                </div>
-
-//                <div className="Mobile">
-//                <FooterMobile/>
-//                </div>
-               
-//           </Fragment>
-//           )
-//      }
-
-
-// export default ProductCategoryPage
-
-     // constructor({match}){
-          // super();
-          // this.state={
-               // Category:match.params.category,
-               // ProductData:[] 
-          // }
-     // }
-
-     // componentDidMount(){
-          // window.scroll(0,0)
+     componentDidMount(){
+          window.scroll(0,0)
           // alert(this.state.Category);
-          // axios.get(AppURL.ProductListByCategory(this.state.Category)).then(response =>{
+          axios.get(AppURL.ProductListByCategory(this.state.Category)).then(response =>{
                
-          //      this.setState({ProductData:response.data});         
+               this.setState({ProductData:response.data});         
 
-          // }).catch(error=>{
+          }).catch(error=>{
 
-          // });
+          });
 
-     // } 
+     } 
+
+     render() {
+          return (
+              <Fragment> 
+               <div className="Desktop">
+                <NavMenuDesktop /> 
+               </div>
+
+               <div className="Mobile">
+               <NavMenuMobile />  
+               </div>                       
+
+               <Category Category={this.state.Category} ProductData={this.state.ProductData} /> 
+               
+               <div className="Desktop">
+               <FooterDesktop/>
+               </div>
+
+               <div className="Mobile">
+               <FooterMobile/>
+               </div>
+               
+          </Fragment>
+          )
+     }
+}
+
+export default ProductCategoryPage

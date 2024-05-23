f0b1932432b8f4b57c13eb2ea02b8b41e86d2bd6
Abolfazl <ab.ghorbani28@gmail.com>
2024-05-23 10:00:25 +0200

    90 Consume Product Details API Part 4

--- frontend/ecom/src/components/ProductDetails/ProductDetails.jsx
+++ frontend/ecom/src/components/ProductDetails/ProductDetails.jsx
@@ -1,82 +1,70 @@
-import React, { useState, useEffect, Fragment } from 'react'
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
-import Product1 from '../../assets/images/product/product1.png'
-import Product2 from '../../assets/images/product/product2.png'
-import Product3 from '../../assets/images/product/product3.png'
-import Product4 from '../../assets/images/product/product4.png'
+import React, { useState, useEffect, Fragment } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const ProductDetails = ({ data }) => {
-     const [productAllData, setProductAllData] = useState({});
-   
-     useEffect(() => {
-       if (data) {
-         setProductAllData(data);
-       }
-     }, [data]);
-   
-     if (!productAllData.productList || !productAllData.productDetails) {
-       return null; // or handle the case where productList or productDetails is not available
-     }
-   
-     const {
-       productList,
-       productDetails
-     } = productAllData;
-   
-     if (!productList[0] || !productDetails[0]) {
-       return null; // or handle the case where the first item in productList or productDetails is not available
-     }
-   
-     const {
-       title,
-       brand,
-       category,
-       subcategory,
-       image,
-       price,
-       product_code,
-       remark,
-       special_price,
-       star
-     } = productList[0]; // Access the first item in the productList array
-   
-     const {
-       image_one,
-       image_two,
-       image_three,
-       image_four,
-       color,
-       size,
-       product_id,
-       short_description,
-       long_description
-     } = productDetails[0]; // Access the first item in the productDetails array
-   
-     // Rest of your component code
-   
+  const [productAllData, setProductAllData] = useState({});
+  const [previewImage, setPreviewImage] = useState('');
+
+  useEffect(() => {
+    if (data) {
+      setProductAllData(data);
+    }
+  }, [data]);
+
+  if (!productAllData.productList || !productAllData.productDetails) {
+    return null; // or handle the case where productList or productDetails is not available
+  }
+
+  const { productList, productDetails } = productAllData;
+
+  if (!productList[0] || !productDetails[0]) {
+    return null; // or handle the case where the first item in productList or productDetails is not available
+  }
+
+  const imgOnClick = (imgSrc) => {
+    setPreviewImage(imgSrc);
+  };
+
+  const {
+    title,
+    brand,
+    category,
+    subcategory,
+    image,
+    price,
+    product_code,
+    remark,
+    special_price,
+    star
+  } = productList[0];
+
+  const {
+    image_one,
+    image_two,
+    image_three,
+    image_four,
+    color,
+    size,
+    product_id,
+    short_description,
+    long_description
+  } = productDetails[0];
 
   return (
     <Fragment>
-      <Container fluid={true} className="BetweenTwoSection">
+      <Container fluid className="BetweenTwoSection">
         <Row className="p-2">
           <Col className="shadow-sm bg-white pb-3 mt-4" md={12} lg={12} sm={12} xs={12}>
             <Row>
               <Col className="p-3" md={6} lg={6} sm={12} xs={12}>
-                <img className="w-100" src={image_one} alt="product" />
+                <img id="previewImg" className="bigimage" src={previewImage || image_one} />
                 <Container className="my-3">
                   <Row>
-                    <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                      <img className="w-100" src={image_one} alt="product" />
-                    </Col>
-                    <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                      <img className="w-100" src={image_two} alt="product" />
-                    </Col>
-                    <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                      <img className="w-100" src={image_three} alt="product" />
-                    </Col>
-                    <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                      <img className="w-100" src={image_four} alt="product" />
-                    </Col>
+                    {[image_one, image_two, image_three, image_four].map((imgSrc, index) => (
+                      <Col key={index} className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
+                        <img onClick={() => imgOnClick(imgSrc)} className="w-100 smallimage product-sm-img" src={imgSrc} />
+                      </Col>
+                    ))}
                   </Row>
                 </Container>
               </Col>
@@ -98,8 +86,7 @@ const ProductDetails = ({ data }) => {
                   <button className="btn btn-primary m-1"> <i className="fa fa-heart"></i> Favourite</button>
                 </div>
               </Col>
-            </Row>
-            <Row>
+              <Row>
               <Col md={6} lg={6} sm={12} xs={12}>
                 <h6 className="mt-2">DETAILS</h6>
                 <p> {long_description} </p>
@@ -114,11 +101,12 @@ const ProductDetails = ({ data }) => {
                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
               </Col>
             </Row>
+            </Row>
           </Col>
         </Row>
       </Container>
     </Fragment>
   );
-}
+};
 
 export default ProductDetails;

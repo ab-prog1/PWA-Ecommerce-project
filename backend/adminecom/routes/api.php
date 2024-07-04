60d4c9b9dc4cdb872b9856d1217e4e5dee40014a
Abolfazl <ab.ghorbani28@gmail.com>
2024-07-04 14:45:31 +0200

    Similar Product Review List

--- backend/adminecom/routes/api.php
+++ backend/adminecom/routes/api.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\User\AuthController;
 use App\Http\Controllers\User\ForgetController;
 use App\Http\Controllers\User\ResetController;
 use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Admin\ReviewController;
 
  /////////////// User Login API Start ////////////////////////
 
@@ -79,3 +80,9 @@ Route::get('/notification', [NotificationController::class, 'NotificationHistory
 
 // Search  route
 Route::get('/search/{key}', [ProductListController::class, 'ProductBySearch']);
+
+// Similar Product Route
+Route::get('/similar/{subcategory}',[ProductListController::class, 'SimilarProduct']);
+
+// Review Product Route
+Route::get('/reviewlist/{id}',[ReviewController::class, 'ReviewList']);

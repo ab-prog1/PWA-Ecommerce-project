59aac2b5aaefd9802bb2d6d0e8559070699bbe37
Abolfazl <ab.ghorbani28@gmail.com>
2024-07-23 14:47:08 +0200

    Product Order History Part 3,1

--- frontend/ecom/src/components/common/Profile.jsx
+++ frontend/ecom/src/components/common/Profile.jsx
@@ -1,29 +1,67 @@
-import React, { Fragment } from "react";
+import React, { Component } from 'react'
+import { Fragment } from 'react'
+import {Navbar,Container, Row, Col,Button,Card,Modal,ListGroup,ListGroupItem} from 'react-bootstrap';
 import { Navigate } from 'react-router';
+import { Link } from 'react-router-dom';
+import Ariyan from '../../assets/images/Untitled4.jpg'
+class Profile extends Component {
+     render() { 
 
-const Profile = ({ user }) => {
-  const name = user ? user.name : "";
-  const email = user ? user.email : "";
-  
+          let name;
+          let email;
+          if(this.props.user){
+               name = this.props.user.name;
+               email = this.props.user.email;
+          }
 
           if(!localStorage.getItem('token')){
                return <Navigate to="/login" />
           }
 
 
+          return (
+              <Fragment> 
+     <div className="section-title text-center mb-55"><h2>User Profile Page</h2>         
+          </div>
+
+          <Container>
+               <Row>
+
+                    <Col lg={4} md={4} sm={12} >
+
+                    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={Ariyan} className="userprofile" />
+ 
+  <ListGroup className="list-group-flush">
+        
+    <ListGroupItem> <Link className="text-link" to="/orderlist"> <p className="product-name-on-card"> Order List </p></Link> </ListGroupItem>
+    
+    <ListGroupItem> <Link className="text-link" to="/orderlist"> <p className="product-name-on-card"> Order List </p></Link> </ListGroupItem>
+    
+    <ListGroupItem> <Link className="text-link" to="/orderlist"> <p className="product-name-on-card"> Order List </p></Link> </ListGroupItem>
+  </ListGroup>
+   
+</Card>
 
 
-  return (
-    <Fragment>
-      <h1> User Profile Page </h1>
+                    </Col>
 
-      <ul className="list-group">
-        <li className="list-group-item">Name : {name} </li>
-        <li className="list-group-item">Email : {email} </li>
-      </ul>
-    </Fragment>
-  );
-};
 
-export default Profile;
+                    <Col lg={8} md={8} sm={12} >
+                    <ul className="list-group">
+<li className="list-group-item">Name :  {name} </li>
+<li className="list-group-item">Email :  {email} </li>
+               </ul>
+                    </Col>
 
+
+               </Row>
+          </Container> 
+
+
+              </Fragment>
+          )
+     }
+}
+
+export default Profile
